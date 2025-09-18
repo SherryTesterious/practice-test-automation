6558eb770009801c6fd79dfd37c8964e6e3d4378
sherry.liew <sherry.testerious@gmail.com>
2025-09-18 09:16:25 +0800

    add: invalid credential test case

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -10,3 +10,12 @@ test('login with valid credentials', async ({ page }) => {
 await expect(page.getByText('Logged In Successfully', { exact: true })).toBeVisible();
 });
 
+test('login with invalid credentials', async ({ page }) => {
+  await page.goto('/practice-test-login/');
+  await page.locator('input[name="username"]').fill("wrongName");
+  await page.locator('input[name="password"]').fill("123456");
+  await page.getByRole('button', { name: /submit/i }).click();
+  await expect(page).toHaveURL(/\/practice-test-login/); 
+  await expect(page.locator('#error')).toHaveText(/Your username is invalid/i);
+});
+
